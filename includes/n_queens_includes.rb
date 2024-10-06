cb4ce90ad3fa42bdd51699e48bdd60d7597e77c0
unixneo <neo@unix.com>
2024-10-06 11:42:44 +0700

    REFACTOR:  Move function out of main code

---
+++ includes/n_queens_includes.rb
@@ -0,0 +1,87 @@
+
+# Turn Seconds into Human-Readable Time with Ruby
+  def human_readable_time(secs)
+    [[60, :seconds], [60, :minutes], [24, :hours], [Float::INFINITY, :days]].map do |count, name|
+      next unless secs > 0
+  
+      secs, number = secs.divmod(count)
+      "#{number.to_i} #{number == 1 ? name.to_s.delete_suffix('s') : name}" unless number.to_i == 0
+    end.compact.reverse.join(', ')
+  end
+
+  def format_time(start_time)
+    time = Time.now - start_time
+    str_time = ''
+    if time > 100
+        str_time = human_readable_time(time.round(6))
+    else
+        str_time = "#{time}s"
+    end
+    str_time
+  end
+  
+  # Function to generate queen's movement array for each position on an NxN board
+  def generate_queen_moves(row, col)
+    board_size = $number_of_queens
+    moves = Array.new(board_size * board_size, 0)
+  
+    # Mark all positions in the same row
+    (0...board_size).each { |c| moves[row * board_size + c] = 1 }
+  
+    # Mark all positions in the same column
+    (0...board_size).each { |r| moves[r * board_size + col] = 1 }
+  
+    # Mark top-left to bottom-right diagonal
+    (-[row, col].min..[board_size - 1 - row, board_size - 1 - col].min).each do |i|
+      moves[(row + i) * board_size + (col + i)] = 1
+    end
+  
+    # Mark top-right to bottom-left diagonal
+    (-[row, board_size - 1 - col].min..[board_size - 1 - row, col].min).each do |i|
+      moves[(row + i) * board_size + (col - i)] = 1
+    end
+  
+    moves
+  end
+  
+  # Function to generate the entire hash of queen moves for all positions on an NxN board
+  def generate_queen_moves_for_n_queens
+    queen_moves = {}
+    board_size = $number_of_queens
+    (0...board_size).each do |row|
+      (0...board_size).each do |col|
+        pos = row * board_size + col
+        queen_moves["Position #{pos}"] = generate_queen_moves(row, col)
+      end
+    end
+    queen_moves
+  end
+  
+  # Function to check if placing a queen at the current position is safe
+  def is_safe(position, queens, queen_moves)
+    queens.each do |q|
+      # Check for conflicts in movement based on queen's attackable positions
+      return false if queen_moves["Position #{q}"][position] == 1
+    end
+    true
+  end
+  
+  # Recursive function to solve the n-queens problem
+  def solve_n_queens(queen_moves, queens = [], row = 0, solutions = [])
+    board_size = $number_of_queens
+    if queens.size == board_size  # All queens placed
+      solutions << queens.dup  # Store the solution
+      return
+    end
+  
+    (0...board_size).each do |col|  # Loop through all columns
+      position = row * board_size + col  # Calculate position based on row and column
+      if is_safe(position, queens, queen_moves)
+        queens.push(position)  # Place the queen
+        solve_n_queens(queen_moves, queens, row + 1, solutions)
+        queens.pop  # Backtrack
+      end
+    end
+  
+    solutions
+  end
--- n_queens.rb
+++ n_queens.rb
@@ -1,71 +1,9 @@
 # Static variable for the number of queens and the board size
-$number_of_queens = 9  # Change this value to solve for different sizes of queens and board (e.g., 8 for 8x8)
+$number_of_queens = 13 # Change this value to solve for different sizes of queens and board (e.g., 8 for 8x8)
+$show_solutions = true # Change this falue to print of the solutions
+start_time = Time.now
 
-# Function to generate queen's movement array for each position on an NxN board
-def generate_queen_moves(row, col)
-  board_size = $number_of_queens
-  moves = Array.new(board_size * board_size, 0)
-
-  # Mark all positions in the same row
-  (0...board_size).each { |c| moves[row * board_size + c] = 1 }
-
-  # Mark all positions in the same column
-  (0...board_size).each { |r| moves[r * board_size + col] = 1 }
-
-  # Mark top-left to bottom-right diagonal
-  (-[row, col].min..[board_size - 1 - row, board_size - 1 - col].min).each do |i|
-    moves[(row + i) * board_size + (col + i)] = 1
-  end
-
-  # Mark top-right to bottom-left diagonal
-  (-[row, board_size - 1 - col].min..[board_size - 1 - row, col].min).each do |i|
-    moves[(row + i) * board_size + (col - i)] = 1
-  end
-
-  moves
-end
-
-# Function to generate the entire hash of queen moves for all positions on an NxN board
-def generate_queen_moves_for_n_queens
-  queen_moves = {}
-  board_size = $number_of_queens
-  (0...board_size).each do |row|
-    (0...board_size).each do |col|
-      pos = row * board_size + col
-      queen_moves["Position #{pos}"] = generate_queen_moves(row, col)
-    end
-  end
-  queen_moves
-end
-
-# Function to check if placing a queen at the current position is safe
-def is_safe(position, queens, queen_moves)
-  queens.each do |q|
-    # Check for conflicts in movement based on queen's attackable positions
-    return false if queen_moves["Position #{q}"][position] == 1
-  end
-  true
-end
-
-# Recursive function to solve the n-queens problem
-def solve_n_queens(queen_moves, queens = [], row = 0, solutions = [])
-  board_size = $number_of_queens
-  if queens.size == board_size  # All queens placed
-    solutions << queens.dup  # Store the solution
-    return
-  end
-
-  (0...board_size).each do |col|  # Loop through all columns
-    position = row * board_size + col  # Calculate position based on row and column
-    if is_safe(position, queens, queen_moves)
-      queens.push(position)  # Place the queen
-      solve_n_queens(queen_moves, queens, row + 1, solutions)
-      queens.pop  # Backtrack
-    end
-  end
-
-  solutions
-end
+require_relative "includes/n_queens_includes"
 
 # Generate the hash for the NxN board
 queen_moves_n_queens = generate_queen_moves_for_n_queens
@@ -74,15 +12,20 @@ queen_moves_n_queens = generate_queen_moves_for_n_queens
 solutions = solve_n_queens(queen_moves_n_queens)
 
 # Print all the solutions
-if solutions.empty?
-  puts "No solutions found"
-else
-  solutions.each_with_index do |solution, index|
-    puts "Solution #{index + 1}:"
-    solution.each do |pos|
-      board_size = $number_of_queens
-      puts "Queen placed at: Position #{pos} (Row #{pos / board_size}, Column #{pos % board_size})"
+if $show_solutions
+  if solutions.empty?
+    puts "No solutions found"
+  else
+    solutions.each_with_index do |solution, index|
+      puts "Solution #{index + 1}:"
+      solution.each do |pos|
+        board_size = $number_of_queens
+        puts "Queen placed at: Position #{pos} (Row #{pos / board_size}, Column #{pos % board_size})"
+      end
+      puts "\n"
     end
-    puts "\n"
   end
 end
+  
+str_time = format_time(start_time)
+puts "Number of solutions: #{solutions.size.to_s.gsub(/\B(?=(...)*\b)/, ',')} for #{$number_of_queens} Queens in #{str_time} \n"
